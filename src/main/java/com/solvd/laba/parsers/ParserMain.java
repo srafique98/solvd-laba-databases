6d89d66d5a3133f56aa5837ed4cf869e0b0d4c04
srafique98 <shanrafique98@gmail.com>
2024-01-15 01:55:45 -0800

    Validated XML file using Stax Parser

--- src/main/java/com/solvd/laba/parsers/ParserMain.java
+++ src/main/java/com/solvd/laba/parsers/ParserMain.java
@@ -5,16 +5,22 @@ import com.solvd.laba.parsers.classes.Transaction;
 import com.solvd.laba.parsers.classes.Account;
 import com.solvd.laba.parsers.classes.Statement;
 import com.solvd.laba.parsers.classes.Loan;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.transform.stax.StAXSource;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -22,10 +28,13 @@ import java.util.List;
 
 public class ParserMain {
     public static void main(String [] args){
-        File file = new File("src/main/resources/parsers/xml/customer.xml");
+        File xmlFile = new File("src/main/resources/parsers/xml/customer.xml");
+        File xsdFile = new File("src/main/resources/parsers/xml/customer.xsd");
         //STAX psrser
+        validateXML(xmlFile, xsdFile);
+
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-        try (FileInputStream fileInputStream = new FileInputStream(file)){
+        try (FileInputStream fileInputStream = new FileInputStream(xmlFile)){
             XMLEventReader reader = xmlInputFactory.createXMLEventReader(fileInputStream);
 
             Customer customer = null;
@@ -151,5 +160,19 @@ public class ParserMain {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private static void validateXML(File xmlFile, File xsdFile) {
+        XMLStreamReader reader = null;
+        try {
+            reader = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(xmlFile));
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(xsdFile);
+            Validator validator = schema.newValidator();
+            validator.validate(new StAXSource(reader));
+            System.out.println("XML and XSD looks good");
+        } catch (XMLStreamException | SAXException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
